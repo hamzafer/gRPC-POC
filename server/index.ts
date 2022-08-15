67970040aa980e35f9b21ef047dfa662366d203f
Hamza Z <hamzafer3@gmail.com>
2022-08-15 19:28:08 +0500

    Added the index file

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import { Server, ServerCredentials } from "grpc";
+import {PluginsService} from "../proto/plugins_grpc_pb";
+import {PluginsServer} from "./services";
+
+const server = new Server();
+server.addService(PluginsService, new PluginsServer());
+
+const port = 4000;
+const uri = `localhost:${port}`;
+console.log(`Listening on ${uri}`);
+server.bind(uri, ServerCredentials.createInsecure());
+
+server.start();
