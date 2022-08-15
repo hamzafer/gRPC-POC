b5de8619394dce8c284eb57639090bb057f1e48e
Hamza Z <hamzafer3@gmail.com>
2022-08-15 20:59:00 +0500

    Added missing ID setter

--- server/plugin.ts
+++ server/plugin.ts
@@ -1,8 +1,9 @@
 import {Plugin, PluginStatus} from "../proto/plugins_pb";
 
-export function pluginToClass({message, status}: Plugin.AsObject) {
+export function pluginToClass({id, message, status}: Plugin.AsObject) {
     const plugin = new Plugin();
 
+    plugin.setId(id);
     plugin.setMessage(message);
     plugin.setStatus(status);
 
