79a3284e2c7c411e01fc04a72af6df7a15531027
Hamza Z <hamzafer3@gmail.com>
2022-08-18 03:34:53 +0500

    Added dockerode script.

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hamza")
+import {DockerHelper} from "./docker/DockerHelper";
+
+const imageTag = 'our_nginx_image';
+const networkName = 'our_network';
+const containerName = 'our_container';
+const dockerHelper = new DockerHelper();
+
+dockerHelper.buildImage(imageTag, process.cwd()).then(() => {
+    console.log('Image built.');
+    return dockerHelper.createNetwork(networkName);
+}).then(() => {
+    console.log('Network created.');
+    return dockerHelper.createContainer(imageTag, containerName, networkName);
+}).then(() => {
+    console.log('Container created.');
+}).catch((err: { stack: any; }) => {
+    console.log(`Something went wrong: ${err.stack}`);
+});
